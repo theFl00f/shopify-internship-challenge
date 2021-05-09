ebd0ade2a0f9b3e7370ac75f845b39871007a890
Celeste Ellerby <celeste.ellerby@gmail.com>
2021-05-08 20:28:50 -0400

    small changes for mobile

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { Home } from "./views/pages/Home";
 
 function App() {
   return (
-    <div className="w-5/6 mx-auto min-w-min py-10">
+    <div className=" w-11/12 md:w-5/6 mx-auto min-w-min py-10">
       <Home />
     </div>
   );
